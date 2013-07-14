bdbcad74d8aa0ab250b8be22d346ca17f8e3cfc4
Joe Rice <riceje7@gmail.com>
2013-07-14 07:44:43 -0400

    initial commit

---
+++ src/lib/game/main.js
@@ -0,0 +1,23 @@
+ig.module('game.main').requires('impact.game', 'impact.font').defines(function () {
+
+	ZombieTD = ig.Game.extend({
+		init: function () {
+			// Initialize your game here; bind keys etc.
+		},
+		update: function () {
+			// Update all entities and backgroundMaps
+			this.parent();
+			// Add your own, additional update code here
+		},
+		draw: function () {
+			// Draw all entities and backgroundMaps
+			this.parent();
+		}
+	});
+
+
+	// Start the Game with 60fps, a resolution of 320x240, scaled
+	// up by a factor of 2
+	ig.main('#canvas', ZombieTD, 60, 960, 640, 1);
+
+});
